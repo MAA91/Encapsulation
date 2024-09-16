ac10edb84eb8a1c5ac1e210cdfe94c9fb3093f59
MAA91 <Sanya13082007@gmail.com>
2024-09-16 18:53:57 +0300

    Add project files.

---
+++ Encapsulation/Point.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    
+    internal class Point
+    {
+        double x;
+        double y;
+
+        public double GetX()
+        {
+            return x;
+        }
+        public void SetX(double x)
+        {
+            this.x = x;
+        }
+        public double GetY()
+        {
+            return y;
+        }
+        public void SetY(double y)
+        {
+            this.y = y;
+        }
+
+        public double X 
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public double Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public Point(double x = 0, double y = 0)
+        {
+            this.x = x;
+            this.Y = y; 
+        }
+
+        public double Distance(Point other)
+        {
+            return Math.Sqrt((this.x - other.x) * (this.x - other.x) + (this.y - other.y) * (this.y - other.y));
+        }
+        public static double Distance(Point A, Point B)
+        {
+            return Math.Sqrt((A.x - B.x) * (A.x - B.x) + (A.y - B.y) * (A.y - B.y));
+        }
+    }
+}
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point A = new Point(2, 3);
+            Point B = new Point(6, 6);
+            Console.WriteLine(A.Distance(B));
+            Console.WriteLine(Point.Distance(A, B));
+        }
+    }
+}
